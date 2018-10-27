15123a963afabcf14dc9edd121b74fbc08d4f9f7
Guillermo Zunino <guillez@gmail.com>
2018-10-27 16:30:44 -0300

    Update version to 0.6.0

--- org.eclipse.swt.chromium/src/org/eclipse/swt/chromium/Chromium.java
+++ org.eclipse.swt.chromium/src/org/eclipse/swt/chromium/Chromium.java
@@ -60,7 +60,7 @@ import org.eclipse.swt.internal.chromium.CEF.cef_client_t;
 import org.eclipse.swt.internal.chromium.CEF.cef_process_id_t;
 
 class Chromium extends WebBrowser {
-    private static final String VERSION = "0500";
+    private static final String VERSION = "0600";
     private static final String CEFVERSION = "3071";
     private static final String SHARED_LIB_V = "chromium_swt-"+VERSION;
     private static final int MAX_PROGRESS = 100;
